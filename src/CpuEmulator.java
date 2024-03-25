9161c7c10f1b319fa85cb5154d8d12f89aa29564
IsmailT0 <ismailtemuroglu2819@gmail.com>
2024-03-25 16:52:30 +0300

    convert to one class format

--- src/CpuEmulator.java
+++ src/CpuEmulator.java
@@ -3,29 +3,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CpuEmulator {
+    private static HashMap<Integer,String> map;
+    private static int AC;
+    private static int PC;
+    private static int FFlag;
+    private static int[] M; //memory
     public static void main(String[] args) throws IOException{
-        CPU cpu = new CPU("program.txt");
-        cpu.run();
-    }
-}
-
-class CPU{
-
-    private HashMap<Integer,String> map;
-    private int AC;
-    private int PC;
-    private int FFlag;
-    private int[] M; //memory
-    public CPU(String filename) throws IOException{
         map = new HashMap<>();
-        readyMap(filename,map);
-        this.AC=0;
-        this.FFlag =0;
-        this.PC=0;
+        AC=0;
+        FFlag =0;
+        PC=0;
         M = new int[256];
+        String filename = "program.txt";//args[0];
+        readyMap(filename,map);
+        run();
     }
-
-    public void run(){
+    public static void run(){
         while (!map.get(PC).equals("start")) {
             PC++;
         }
@@ -50,7 +43,7 @@ class CPU{
                 case "cmpm" -> cmpM(num);
                 case "cjmp" -> cJMP(num);
                 case "jmp" -> jmp(num);
-                case "add" -> add(num   );
+                case "add" -> add(num);
                 case "addm" -> addM(num);
                 case "subm" -> subM(num);
                 case "sub" -> sub(num);
@@ -94,65 +87,63 @@ class CPU{
         }
     }
 
-    //Instruction functions
-    private void load(int x){
-        this.AC = x;
+
+    private static void load(int x){
+        AC = x;
     }
 
-    private void loadM(int x){
-        this.AC=M[x];
+    private static void loadM(int x){
+        AC=M[x];
     }
 
-    private void store(int x){
-        this.M[x] =this.AC;
+    private static void store(int x){
+        M[x]= AC;
     }
 
-    private void cmpM(int x){
+    private static void cmpM(int x){
         int diff = AC - M[x];
 
         if (diff > 0)
-            this.FFlag=1;
+            FFlag=1;
         else if (diff < 0 )
-            this.FFlag=-1;
+            FFlag=-1;
         else
-            this.FFlag = 0;
+            FFlag = 0;
     }
 
-    private void cJMP(int x){
+    private static void cJMP(int x){
         if (FFlag == 1)
-            this.PC = x-1;
+            PC = x-1;
     }
 
-    private void jmp(int x){
-        this.PC = x-1;
+    private static void jmp(int x){
+        PC = x-1;
     }
 
-    private void add(int x){
-        this.AC =  AC + x;
+    private static void add(int x){
+        AC =  AC + x;
     }
 
-    private void addM(int x){
-        this.AC = AC + M[x];
+    private static void addM(int x){
+        AC = AC + M[x];
     }
 
-    private void subM(int x){
-        this.AC = AC - M[x];
+    private static void subM(int x){
+        AC = AC - M[x];
     }
 
-    private void sub(int x){
-        this.AC = AC - x;
+    private static void sub(int x){
+        AC = AC - x;
     }
 
-    private void mul(int x){
-        this.AC = AC*x;
+    private static void mul(int x){
+        AC = AC*x;
     }
-    private void mulM(int x){
-        this.AC = AC * M[x];
+    private static void mulM(int x){
+        AC = AC * M[x];
     }
 
-    private void disp(){
+    private static void disp(){
         System.out.println(AC);
     }
-
-
 }
